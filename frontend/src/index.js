69280e516aa2eca48f03b586e8a6a2beee84f088
davesahaj <sahajdavework@pm.me>
2022-04-01 18:32:21 +0530

    added Redux store provider

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
