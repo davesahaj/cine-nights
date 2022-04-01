87c8e44a58a043011219830897652c56da7406ff
davesahaj <sahajdavework@pm.me>
2022-04-01 18:27:15 +0530

    configured default colors for console

---
+++ backend/ColorTheme.js
@@ -0,0 +1,14 @@
+const colorTheme = {
+  silly: "rainbow",
+  input: "grey",
+  verbose: "cyan",
+  prompt: "grey",
+  info: "green",
+  data: "grey",
+  help: "cyan",
+  warn: "yellow",
+  debug: "blue",
+  error: "red",
+};
+
+module.exports = { colorTheme };
