86f15a1c2446ca282253d08abe4e6dc41385b7ad
Rushiraj-dave <rushirajdave111@gmail.com>
2022-04-03 14:11:11 +0530

    added videoJS player

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
+//Routes
+const roomRoute = require("./routes/roomRoute");
+app.use("/", roomRoute);
 
 module.exports = app;
